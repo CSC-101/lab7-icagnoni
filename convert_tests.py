3d24471dd9d2c926bae93d67ad811de8d5806592
iacopocagnoni <icagnoni@calpoly.edu>
2024-11-10 19:03:38 -0800

    Commit 1

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,11 @@
+import convert
+import unittest
 
+class TestStrToFloat(unittest.TestCase):
+    # Task 1
+
+    def test_str_to_float(self):
+        self.assertEqual(convert.str_to_float("10"), 10)
+
+    def test_invalid_string(self):
+        self.assertIsNone(convert.str_to_float("abc"))
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,15 @@
+from typing import List
+from convert import str_to_float
 
+def gather_numbers() -> List[float]:
+    numbers = []
+    while True:
+        user_input = input("Enter a number: ")
+        if user_input.lower() == "done":
+            break
+        number = str_to_float(user_input)
+        if number is not None:
+            numbers.append(number)
+        else:
+            print("Invalid. Enter a valid number.")
+    return numbers
